c445e8408f6ed48b66b44761024d645373126bd2
Andre <andreivanp@gmail.com>
2022-11-16 20:01:34 +0700

    fix function

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const num = fizzBuzz(15);
+// function fizzBuzz(param) {
+//   if (param % 15 == 0) {
+//     console.log("Fizz Buzz");
+//   } else if (param % 5 == 0) {
+//     console.log("Buzz");
+//   } else if (param % 3 == 0) {
+//     console.log("Fizz");
+//   } else {
+//     console.log("Unknown");
+//   }
+//   return fizzBuzz;
+// }
 
 function fizzBuzz(param) {
-	if (param % 15 == 0) {
-		console.log("Fizz Buzz");
-	} else if (param % 5 == 0) {
-		console.log("Buzz");
-	} else if (param % 3 == 0) {
-		console.log("Fizz");
-	} else {
-		console.log("Unknown");
-	}
-	return fizzBuzz;
+  let result = "";
+  if (param % 15 == 0) {
+    result = "Fizz Buzz";
+  } else if (param % 5 == 0) {
+    result = "Buzz";
+  } else if (param % 3 == 0) {
+    result = "Fizz";
+  } else {
+    return param;
+  }
+  return result;
 }
+
+const num = fizzBuzz(15);
+
+console.log(num);
